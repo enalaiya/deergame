91303ecca94b6ef097b364ba5374cd34fb852b5d
Lizzy <lizzyschoen@gmail.com>
2015-11-04 20:50:21 -0800

    initial commit - adding all my files

---
+++ underwater.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+var waterLevel : float;
+private var isUnderwater : boolean;
+private var normalColor : UnityEngine.Color;
+private var underwaterColor : UnityEngine.Color;
+
+
+
+function Start () 
+{
+
+	normalColor = new Color (0.5f, 0.5f, 0.5f, 0.5f);
+	underwaterColor = new Color (0.22f, 0.65f, 0.77f, 0.5f);
+
+}
+
+function Update () {
+
+	if((transform.position.y < waterLevel) != isUnderwater)
+	
+	{ 
+		isUnderwater = transform.position.y < waterLevel;
+		if (isUnderwater) setUnderwater ();
+		if (!isUnderwater) setNormal ();
+	}
+}
+
+function setNormal ()
+{
+	RenderSettings.fogColor = normalColor;
+	RenderSettings.fogDensity = 0.001f;
+}
+
+function setUnderwater ()
+{
+	RenderSettings.fogColor = underwaterColor;
+	RenderSettings.fogDensity = 0.03f;
+}
+
